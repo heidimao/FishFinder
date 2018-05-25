5051b09f992d2e8e585aa32770e8b1e5cb4cc215
Heidi Mao <heidimao@gmail.com>
2018-05-25 19:33:23 +0200

    Update
    Trying to make the user input match the JSON data.....

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,32 @@ var fishlistJSON= require('../fishes.json');
 
 //routes
 exports.home =function(req, res){
-	var fishList = fishlistJSON.fishes;
-	
+	var fishlist= fishlistJSON.fishes;
 	res.render('home',{
-		
-		
-		fishList: fishList
-
-
+		title: 'fish',
+		fish: fishlist
 	});
+
 };
 
+exports.find= function(req, res){
+	var fishlist= fishlistJSON.fishes;
+	
+	var fish_name= req.body.fish_name;
+	if(fish_name == fishlist.name){
+		res.send("sucess")
+	}else{
+		res.send("fail")
+	}
+
+	
+	
+};
 exports.fishFound= function(req, res){
-	res.render('fishFound'),
+	res.render('fishFound')
 };
 
 exports.fishNotFound = function(req,res){
-	res.render('fishNotFound'),
+	res.render('fishNotFound')
 };
+
