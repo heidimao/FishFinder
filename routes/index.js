9b5cb6a46997108336bcee141187daf70ad43884
Heidi Mao <heidimao@gmail.com>
2018-05-26 23:14:17 +0200

    Update: Searching
    Trying to make that user can search the fishname by entering both lower and upper cases by converting the first character into Uppercases

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var fishlistJSON= require('../fishes.json');
 exports.home =function(req, res){
 	var fishlist= fishlistJSON.fishes;
 	res.render('home',{
-		title: 'fish',
+		title: 'Find Fish',
 		fish: fishlist
 	});
 
@@ -13,24 +13,38 @@ exports.home =function(req, res){
 
 exports.find= function(req, res){
 	var fishlist= fishlistJSON.fishes;
-	
 	var fish_name= req.body.fish_name;
-	
-	
-	
-	for (var i =0; i< fishlist.length; i++){
-		if (fishlist[i].name == fish_name){
-			res.send(fishlist[i].desc);
-			
+	String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+	var Adjusted= fish_name.capitalize()
+
+	for (var i=0; i<fishlist.length; i++){
+		
+		if(Adjusted===fishlist[i].name){
+			console.log(Adjusted);
+			var Name = fishlist[i].name;
+			var Status = fishlist[i].status;
+			var Image= fishlist[i].image;
+			var Des = fishlist[i].desc;
+			var Price= fishlist[i].price;
+			res.render('fishFound', {
+				name: Name,
+				status: Status,
+				image: Image,
+				desc: Des,
+				price: Price,
+
+			});
 		};
+
 	};
 	
+	
+	
 
-	
-
-	
-	
 };
+
 exports.fishFound= function(req, res){
 	res.render('fishFound')
 };
